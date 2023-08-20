37818f2fe3ac831d8df792401f3f41bdbcca5484
David Solis <h.jr72xeofj9u@gmail.com>
2023-08-20 17:28:11 +0000

    Finished The Simple Bots

--- simplebot.py
+++ simplebot.py
@@ -1,11 +1,11 @@
 import tkinter.scrolledtext as tks #creates a scrollable text window
+import os
+import openai
+
+openai.api_key=os.getenv('OPENAI_KEY')
 
 from datetime import datetime
 from tkinter import *
-
-import os
-import openai
-openai.api_key=os.getenv('OPENAI_KEY')
 
 # Generating response
 def get_bot_response(user_input):
@@ -22,61 +22,8 @@ def get_bot_response(user_input):
     bot_response = response.choices[0].text.strip()
     return bot_response
 
-# Create the main application window using Tk()
-baseWindow = Tk()
 
-# Set the title of the window
-baseWindow.title("The Simple Bot")
 
-# Set the size of the window
-baseWindow.geometry("500x250")
-
-# Create the chat window as a ScrolledText widget with "Arial" font
-chatWindow = tks.ScrolledText(baseWindow, font="Arial")
-
-# Configure tags for message alignment: 'tag-left' for bot messages, 'tag-right' for user messages
-chatWindow.tag_configure('tag-left', justify='left')
-chatWindow.tag_configure('tag-right', justify='right')
-
-# Disable the chat window initially (it should not be editable by the user)
-chatWindow.config(state=DISABLED)
-
-def send(event):
-    chatWindow.config(state=NORMAL)
-
-    user_input = userEntryBox.get("1.0",'end-2c')
-    user_input_lc = user_input.lower()
-    bot_response = get_bot_response(user_input_lc) 
-
-    create_and_insert_user_frame(user_input)
-    create_and_insert_bot_frame(bot_response)
-
-    chatWindow.config(state=DISABLED)
-    userEntryBox.delete("1.0","end")
-    chatWindow.see('end')
-
-# Create the send button, with specific font, text, and background color
-sendButton = Button(
-    baseWindow,
-    font=("Verdana", 12, 'bold'),
-    text="Send",
-    bg="#fd94b4",
-    activebackground="#ff467e",
-    fg='#ffffff',
-    command=send)
-sendButton.bind("<Button-1>", send)
-baseWindow.bind('<Return>', send)
-
-# Create the user entry box where the user types their messages
-userEntryBox = Text(baseWindow, bd=1, bg="white", width=38, font="Arial")
-
-# Place the chat window, user entry box, and send button on the main window using specific coordinates and sizes
-chatWindow.place(x=1, y=1, height=200, width=500)
-userEntryBox.place(x=3, y=202, height=27)
-sendButton.place(x=430, y=200)
-
-# Start the main event loop to keep the application running and responsive
-baseWindow.mainloop()    
 
 def create_and_insert_user_frame(user_input):
   userFrame = Frame(chatWindow, bg="#d0ffff")
@@ -117,3 +64,49 @@ def create_and_insert_bot_frame(bot_response):
   chatWindow.window_create('end', window=botFrame)
   chatWindow.insert(END, "\n\n" + "")
 
+
+
+
+def send(event):
+    chatWindow.config(state=NORMAL)
+
+    user_input = userEntryBox.get("1.0",'end-2c')
+    user_input_lc = user_input.lower()
+    bot_response = get_bot_response(user_input_lc) 
+
+    create_and_insert_user_frame(user_input)
+    create_and_insert_bot_frame(bot_response)
+
+    chatWindow.config(state=DISABLED)
+    userEntryBox.delete("1.0","end")
+    chatWindow.see('end')
+
+
+
+baseWindow = Tk()
+baseWindow.title("The Simple Bot")
+baseWindow.geometry("500x250")
+
+chatWindow = tks.ScrolledText(baseWindow, font="Arial")
+chatWindow.tag_configure('tag-left', justify='left')
+chatWindow.tag_configure('tag-right', justify='right')
+chatWindow.config(state=DISABLED)
+
+sendButton = Button(
+    baseWindow,
+    font=("Verdana", 12, 'bold'),
+    text="Send",
+    bg="#fd94b4",
+    activebackground="#ff467e",
+    fg='#ffffff',
+    command=send)
+sendButton.bind("<Button-1>", send)
+baseWindow.bind('<Return>', send)
+
+userEntryBox = Text(baseWindow, bd=1, bg="white", width=38, font="Arial")
+
+chatWindow.place(x=1, y=1, height=200, width=500)
+userEntryBox.place(x=3, y=202, height=27)
+sendButton.place(x=430, y=200)
+
+baseWindow.mainloop()        
